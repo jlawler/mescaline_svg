0ccc31b52d070bab30eecddbbdfdd71e2026365b
Jeremy Lawler (papyrus) <jeremylawler@gmail.com>
2010-03-07 20:45:24 -0600

    autocommit

--- demo/mescaline.js
+++ demo/mescaline.js
@@ -1,6 +1,12 @@
-mescaline=function(){
+
+var graph = function(){
+
+
+}; 
+mescaline=function(rn){
   var mescaline_struct={
-    graph_group: new Array({})
+    graph_group: new Array({}),
+    root_node: rn
   };
   return {
     graph_group: function(){return mescaline_struct['graph_group'][0]}
