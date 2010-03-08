ee3c46a2f9f086a828dfc24f260da939970e1fe5
Jeremy Lawler (papyrus) <jeremylawler@gmail.com>
2010-03-07 21:43:44 -0600

    autocommit

--- demo/mescaline.js
+++ demo/mescaline.js
@@ -24,7 +24,7 @@ node.setAttribute('style','stroke: black;  stroke-width: 5.2;');
 alert(coerce_data(data));
 node.setAttribute('points',coerce_data(data));
 node.setAttribute('x',0);
-node.setAttribute('y','100%');
+node.setAttribute('y',0);
 pnode.appendChild(node)     ;
     }
   };
