d408f3431ca3ee9b75537ea5f41472b7e182a8dc
Jeremy Lawler (papyrus) <jeremylawler@gmail.com>
2010-03-07 21:27:48 -0600

    autocommit

--- demo/mescaline.js
+++ demo/mescaline.js
@@ -18,7 +18,7 @@ pair=din[i];
   var first_gg = {
     add_graph: function(data,opts){
 var pnode = document.getElementById('tle');
-var node=document.createElementNS('http://www.w3.org/2000/svg','polyline');
+var node=document.createElementNS('http://www.w3.org/2000/svg','line');
 node.setAttribute('style','stroke: black;fill: red; fill-opacity: 0.3; stroke-width: 0.2;');
 //alert(coerce_data(data));
 node.setAttribute('points',coerce_data(data));
